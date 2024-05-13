67297dbadb594f9926fe08437c4be9ed52e6da93
somsomso <cksnzm28@gmail.com>
2024-05-13 13:15:41 +0900

    https://github.com/ALOHA-CLASS/SPRING_BOARD_TEST.git
    Signed-off-by: somsomso <cksnzm28@gmail.com>

---
+++ board/SQL/DDL.sql
@@ -0,0 +1,13 @@
+-- Active: 1715220502910@@127.0.0.1@3306@joeun
+DROP TABLE board;
+
+CREATE TABLE `board` (
+  `no` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `writer` varchar(100) NOT NULL,
+  `content` text,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `views` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`no`)
+) COMMENT='게시판';
